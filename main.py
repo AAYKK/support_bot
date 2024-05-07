199a4181aaf19c51a22683ad50a76dae7b5e24ab
AAYKK <116094602+AAYKK@users.noreply.github.com>
2024-05-07 19:30:33 +0300

    Update main.py
    Исправления №1

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from functools import partial
 
 bot=tb.TeleBot(token='7104012758:AAHPvjgf9XwTzRDg3lHuLO2qq0pbdfLC_28')
 
+products=["Крипто-ПРО","1С","ЭЦП","Чат-бот"]
+
 @bot.message_handler(commands=['start'])
 def start(message):
     #bot.send_message(message.chat.id, '',reply_markup=types.ReplyKeyboardRemove())
@@ -12,7 +14,7 @@ def start(message):
     btn2 = types.KeyboardButton("🛈 Информация о боте")
     markup.add(btn1, btn2)
     
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}!\n Чем могу вам помочь?".format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text="{0.first_name}!\n Чем могу вам помочь?".format(message.from_user), reply_markup=markup)
     
     bot.register_next_step_handler(message, step_1)
 
@@ -30,87 +32,121 @@ def step_1(message):
         
     elif message.text == "❓ Задать вопрос":
         #bot.send_message(message.chat.id, '',reply_markup=types.ReplyKeyboardRemove())
-        btn_crypto = types.KeyboardButton("Крипто-ПРО")
-        btn_1C = types.KeyboardButton("1С")
-        btn_CP = types.KeyboardButton("ЭЦП")
-        btn_chat = types.KeyboardButton("Чат-бот")
-        btn_other= types.KeyboardButton("Другое")
-        
-        markup.add(btn_crypto,btn_1C,btn_CP,btn_chat,btn_other)
-        
-        bot.send_message(message.chat.id, text="Выберите тему вопроса", reply_markup=markup)
-        
-        bot.register_next_step_handler(message, step_2)
-        
+        product_change(message,markup)
     
-
+    else:
+        bot.send_message(message.chat.id, text="Неверный ввод. Попробуйте еще раз.")
+        bot.register_next_step_handler(message, step_1)
+                
+        
+        
+        
+def product_change(message,markup=None):
+    if markup==None:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  
+    for product in products:
+        markup.add(types.KeyboardButton(product))
+        
+    markup.add(types.KeyboardButton("Другое"))
+    
+    bot.send_message(message.chat.id, text="Выберите тему вопроса", reply_markup=markup)
+    
+    bot.register_next_step_handler(message, step_2)
+    
+    
+    
 def step_2(message):
-    if message.text=='В главное меню':
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    
+    if  message.text=='/start':
+        start(message)
+        
+    elif message.text=='В главное меню':
         bot.register_next_step_handler(message, start)
     
     elif message.text=='Другое':
           operator(message)
-          
+    
+    elif message.text not in products:
+        bot.send_message(message.chat.id, text="У нас нет такого продукта.\nЕсли темы вашего вопроса нет в списке, то нажмите кнопку <Другое>")   
+        product_change(message,markup)
+        
     else:
         product=message.text
+        product_actions(message, product)
+
+
+def product_actions(message, product, markup=None, change=None):
+    
+    if markup==None:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1=types.KeyboardButton("Информация о продукте")
-        btn2=types.KeyboardButton("Прайс")
-        btn3=types.KeyboardButton("Консультация")
-        back = types.KeyboardButton("В главное меню")
-        markup.add(btn1,btn2,btn3,back)
-        bot.send_message(message.chat.id, text="Что вас интерсует?", reply_markup=markup)
         
-        bot.register_next_step_handler(message, partial(step_3, product=product))
+    actions=["Информация о продукте", "Прайс","Консультация"]
+    
+    if change:
+        actions.remove(change)
+        text="Что вас ещё интерсует?"
+    else:
+        text="Что вас интерсует?"
+        
+    print(f"\n\n\n\actions={actions}\n\n\n")
+    
+    for action in actions:
+        markup.add(types.KeyboardButton(action))
 
+    markup.add(types.KeyboardButton("В главное меню"))
 
+    bot.send_message(message.chat.id, text=text, reply_markup=markup)
+
+    bot.register_next_step_handler(message, partial(step_3, product=product))
+    
+    
+    
 def step_3(message, product):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    if message.text=='В главное меню':
+    if  message.text=='/start' or message.text=='В главное меню':
         start(message)
-    
+        
     elif message.text=="Консультация":
         operator(message, product=product)
-            
-    else:
+           
+    elif message.text not in ['Информация о продукте','Прайс']: 
+        bot.send_message(message.chat.id, text=f"Я вас не понимаю. Выберите вариант ответа из предложенных")
+        product_actions(message, product, markup)
+        
+    else: 
         if message.text=='Информация о продукте':
             bot.send_message(message.chat.id, text=f"Здесь будет информация про {product}")
-            
-            btn1=types.KeyboardButton("В главное меню")
-            btn2=types.KeyboardButton("Прайс")
-            btn3=types.KeyboardButton("Консультация")
-            
-            markup.add(btn1,btn2,btn3)
 
         elif message.text=='Прайс':
             bot.send_message(message.chat.id, text=f"Здесь будет информация про прайс для {product}")
 
-            btn1=types.KeyboardButton("Информация о продукте")
-            btn2=types.KeyboardButton("Консультация")
-            btn3=types.KeyboardButton("В главное меню")
-
-            markup.add(btn1,btn2,btn3)
-        
-        bot.send_message(message.chat.id, text=f"Хотите узнать что-то ещё про {product}?", reply_markup=markup)
-
-        bot.register_next_step_handler(message, partial(step_3, product=product))
+        product_actions(message, product, markup, message.text)
 
 
+                
 def operator(message, product=None):
+    def process_question(message):
+        if message.text=="В главное меню" or message.text=="/start":
+          start(message)
+          
+        else:
+            # Здесь можно обработать вопрос пользователя, например, сохранить его в базу данных
+            bot.send_message(message.chat.id, text="Ваш вопрос передан специалисту")
+            start(message)
+            
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     
     if product:
-        bot.send_message(message.chat.id, text=f"Напишите ваш вопрос касательно {product}, я передам его специалисту",reply_markup=types.ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, text=f"Напишите ваш вопрос касательно {product}, я передам его специалисту",reply_markup=markup.add(types.KeyboardButton("В главное меню")))
     
     else:
-        bot.send_message(message.chat.id, text=f"Напишите ваш вопрос и я передам его специалисту",reply_markup=types.ReplyKeyboardRemove())
+        bot.send_message(message.chat.id, text=f"Напишите ваш вопрос и я передам его специалисту",reply_markup=markup.add(types.KeyboardButton("В главное меню")))
     
     bot.register_next_step_handler(message, process_question)
 
-def process_question(message):
-    # Здесь можно обработать вопрос пользователя, например, сохранить его в базу данных
 
-    bot.send_message(message.chat.id, text="Ваш вопрос передан специалисту")
-    start(message)
     
 bot.polling(none_stop=True, interval=0)
